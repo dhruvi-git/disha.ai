58a5e55aaf4dc2b0a4ccc3216582407ceb6a0f96
Dhruvi Jain <dhruvijain2019@gmail.com>
2026-03-20 15:24:02 +0530

    dashboard

--- frontend/actions/dashboard.js
+++ frontend/actions/dashboard.js
@@ -28,7 +28,7 @@ export const generateAIInsights = async (industry) => {
         `;
 
   const result = await groq.chat.completions.create({
-    model: "llama3-70b-8192",
+    model: "llama-3.3-70b-versatile",
     messages: [{ role: "user", content: prompt }],
     response_format: { type: "json_object" },
   });
